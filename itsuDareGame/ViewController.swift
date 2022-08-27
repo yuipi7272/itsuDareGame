760623e78d3ba62c596b322335655d785ec52010
YuiOgawa <yui20010218@gmail.com>
2022-08-27 10:30:06 +0900

    :art: スキルチェック2に挑戦
    - 「いつ」「どこで」「だれが」「どうした」の要素をそれぞれ4つ→6つへ

--- itsuDareGame/ViewController.swift
+++ itsuDareGame/ViewController.swift
@@ -14,10 +14,10 @@ class ViewController: UIViewController {
     @IBOutlet var daregaLabel: UILabel!
     @IBOutlet var doshitaLabel: UILabel!
     
-    let itsuArray: [String] = ["一年前", "一週間前", "昨日", "今日"]
-    let dokodeArray: [String] = ["山の上で", "アメリカで", "学校で", "クラスで"]
-    let daregaArray: [String] = ["僕が", "大統領が", "先生が", "友達が"]
-    let doshitaArray: [String] = ["叫んだ", "演説した", "怒った", "踊った"]
+    let itsuArray: [String] = ["一年前", "一週間前", "昨日", "今日", "一分前", "千年前"]
+    let dokodeArray: [String] = ["山の上で", "アメリカで", "学校で", "クラスで", "スーパーで", "庭で"]
+    let daregaArray: [String] = ["僕が", "大統領が", "先生が", "友達が", "母が", "平安貴族が"]
+    let doshitaArray: [String] = ["叫んだ", "演説した", "怒った", "踊った", "買い物をした", "和歌を詠んだ"]
     
     var index:Int = 0
     
@@ -34,7 +34,7 @@ class ViewController: UIViewController {
         doshitaLabel.text = doshitaArray[index]
         
         // indexの値に+1
-        if index == 3 {
+        if index == 5 {
             index = 0
         } else {
             index = index + 1
@@ -54,10 +54,10 @@ class ViewController: UIViewController {
     
     @IBAction func random(){
         // 乱数を生成し、それぞれのindexに入れる
-        let itsuIndex = Int.random(in: 0...3)
-        let dokodeIndex = Int.random(in: 0...3)
-        let daregaIndex = Int.random(in: 0...3)
-        let doshitaIndex = Int.random(in: 0...3)
+        let itsuIndex = Int.random(in: 0...5)
+        let dokodeIndex = Int.random(in: 0...5)
+        let daregaIndex = Int.random(in: 0...5)
+        let doshitaIndex = Int.random(in: 0...5)
         
         print("いつ：\(itsuIndex)")
         print("どこで：\(dokodeIndex)")
